533453083b3679ce5bb0eebd87415eb652296626
Dan Zabinski <dzabinski@peerless-av.com>
2016-10-19 12:56:33 -0500

    entered logic for reading file from redirect.

--- APSoap/APSoap/AddSoap.cpp
+++ APSoap/APSoap/AddSoap.cpp
@@ -6,7 +6,13 @@ using namespace std;
 
 int main()
 {
+	char c;   // from      http://stackoverflow.com/questions/13559462/using-input-redirection-how-to-read-file-and-get-character-string-in-c
+	while (std::cin.get(c))
+	{
+		std::cout << c << std::endl; // will print out each character on a new line
+	}
 
 	return 0;
 }
+
 
